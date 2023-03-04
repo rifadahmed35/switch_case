e6932f7b1d10bb67e2ac894466d6bea0a609afbf
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-05 00:35:48 +0100

    Switch case project

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=3;
+        switch (x) {
+            case 1: {
+                System.out.println("France");
+                break;
+            }
+            case 2: {
+                System.out.println("England");
+                break;
+            }
+            case 3: {
+                System.out.println("Germany");
+                break;
+            }
+            case 10: {
+                System.out.println("Finlad");
+            }
+        }
+    }
+}
